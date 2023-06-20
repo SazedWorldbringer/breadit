57b727bf665f7a6249df5681f4e21727bd06e69e
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-20 19:59:00 +0530

    feat: implement dropdown menu with user avatar as the trigger, display some links

---
+++ src/components/user-account-nav.tsx
@@ -0,0 +1,60 @@
+import { FC } from "react"
+import { User } from "next-auth"
+
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem } from "@/components/ui/dropdown-menu"
+import UserAvatar from "@/components/user-avatar"
+import Link from "next/link"
+
+interface UserAccountNavProps {
+	user: Pick<User, 'name' | 'image' | 'email'>
+}
+
+const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger>
+				<UserAvatar
+					className="h-8 w-8"
+					user={{
+						name: user.name || null, image: user.image || null
+					}}
+				/>
+			</DropdownMenuTrigger>
+
+			<DropdownMenuContent className="bg-card" align="end">
+				<div className="flex items-center justify-start gap-2 p-2">
+					<div className="flex flex-col space-y-1 leading-none">
+						{user.name && <p className="font-medium">{user.name}</p>}
+						{user.email && (
+							<p className="w-[200px] truncate text-sm">{user.email}</p>
+						)}
+					</div>
+				</div>
+
+				<DropdownMenuSeparator />
+
+				<DropdownMenuItem asChild>
+					<Link href='/'>Feed</Link>
+				</DropdownMenuItem>
+
+				<DropdownMenuItem asChild>
+					<Link href='/r/create'>Create community</Link>
+				</DropdownMenuItem>
+
+				<DropdownMenuItem asChild>
+					<Link href='/settings'>Settings</Link>
+				</DropdownMenuItem>
+
+				<DropdownMenuSeparator />
+
+				<DropdownMenuItem
+					className="cursor-pointer"
+				>
+					Sign out
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	)
+}
+
+export default UserAccountNav
