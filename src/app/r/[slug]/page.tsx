253d22b68b4f6ba02fb070e49476b835a029414b
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-24 19:43:41 +0530

    feat: display a create post form that redirects user to submit r/[slug]/submit page

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation"
 import { getAuthSession } from "@/lib/auth"
 import { db } from "@/lib/db"
 import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config"
+import MiniCreatePost from "@/components/mini-create-post"
 
 interface PageProps {
 	params: {
@@ -38,6 +39,7 @@ const page = async ({ params }: PageProps) => {
 			<h1 className="font-bold text-3xl md:text-4xl h-14">
 				r/{subreddit.name}
 			</h1>
+			<MiniCreatePost session={session} />
 		</>
 	)
 }
---
+++ src/components/mini-create-post.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { FC } from 'react'
+import { Session } from 'next-auth'
+import { usePathname, useRouter } from 'next/navigation'
+
+import UserAvatar from '@/components/user-avatar'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { Icons } from "@/components/icons";
+
+interface MiniCreatePostProps {
+	session: Session | null
+}
+
+const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
+	const router = useRouter()
+	const pathname = usePathname()
+
+	return (
+		<li className='overflow-hidden rounded-md bg-white shadow'>
+			<div className='h-full px-6 py-4 flex justify-between gap-6'>
+				<div className='relative'>
+					<UserAvatar user={{
+						name: session?.user.name || null,
+						image: session?.user.image || null,
+					}}
+					/>
+
+					<span className='absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white' />
+				</div>
+
+				<Input
+					readOnly
+					onClick={() => router.push(pathname + '/submit')}
+					placeholder='Create post'
+				/>
+
+				<Button
+					onClick={() => router.push(pathname + '/submit')}
+					variant='ghost'
+				>
+					<Icons.image className="h-4 w-4" />
+				</Button>
+
+				<Button
+					onClick={() => router.push(pathname + '/submit')}
+					variant='ghost'
+				>
+					<Icons.link className="h-4 w-4" />
+				</Button>
+			</div>
+		</li>
+	)
+}
+
+export default MiniCreatePost
