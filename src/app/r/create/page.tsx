86a707a3a92816424169e59055902c1b43822b24
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-24 16:00:42 +0530

    feat: handle error status codes 409(already exists) and 422(invalid name)

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -4,16 +4,18 @@ import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 import { useMutation } from '@tanstack/react-query'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 
 import { CreateSubredditPayload } from '@/lib/validators/subreddit'
-
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { toast } from '@/components/ui/use-toast'
+import { useCustomToast } from '@/hooks/use-custom-toast'
 
 const page = () => {
 	const [input, setInput] = useState<string>('')
 	const router = useRouter()
+	const { loginToast } = useCustomToast()
 
 	const { mutate: createCommunity, isLoading } = useMutation({
 		mutationFn: async () => {
@@ -22,6 +24,25 @@ const page = () => {
 			}
 			const { data } = await axios.post('/api/subreddit', payload)
 			return data as string
+		},
+		onError: (err) => {
+			if (err instanceof AxiosError) {
+				if (err.response?.status === 409) {
+					return toast({
+						title: 'Subreddit already exists',
+						description: 'Please choose a different subreddit name.',
+						variant: 'destructive'
+					})
+				}
+
+				if (err.response?.status === 422) {
+					return toast({
+						title: 'Invalid subreddit name',
+						description: 'Please choose a name between 3 and 21 characters.',
+						variant: 'destructive'
+					})
+				}
+			}
 		},
 	})
 
