24c9b122ce3e1a96223a0a7fe38edaed7ee2cf40
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-26 22:20:39 +0530

    feat: import editorjs plugins in the useCallback hook, configure link tool

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import { FC } from 'react';
+import { FC, useCallback, useRef } from 'react';
 import TextareaAutosize from 'react-textarea-autosize'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import type EditorJS from '@editorjs/editorjs';
 
 import { PostCreationRequest, PostValidator } from '@/lib/validators/post';
 
@@ -25,6 +26,41 @@ const Editor: FC<editorProps> = ({ subredditId }) => {
 			content: null
 		},
 	})
+
+	const ref = useRef<EditorJS>()
+
+	const initializeEditor = useCallback(async () => {
+		const EditorJS = (await import('@editorjs/editorjs')).default
+		const Header = (await import('@editorjs/header')).default
+		const Embed = (await import('@editorjs/embed')).default
+		const Table = (await import('@editorjs/table')).default
+		const List = (await import('@editorjs/list')).default
+		const Code = (await import('@editorjs/code')).default
+		const LinkTool = (await import('@editorjs/link')).default
+		const InlineCode = (await import('@editorjs/inline-code')).default
+		const ImageTool = (await import('@editorjs/image')).default
+
+		if (!ref.current) {
+			const editor = new EditorJS({
+				holder: 'editor',
+				onReady() {
+					ref.current = editor
+				},
+				placeholder: 'Type here to write your post...',
+				inlineToolbar: true,
+				data: { blocks: [] },
+				tools: {
+					header: Header,
+					linkTool: {
+						class: LinkTool,
+						config: {
+							endpoint: '/api/link',
+						},
+					},
+				},
+			})
+		}
+	}, [])
 
 	return (
 		<div className='w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200'>
