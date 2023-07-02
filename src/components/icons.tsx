4ca2134cac765154f2d974f3aecffb5eec776cdc
Atharva Pardeshi <atharva.exe@gmail.com>
2023-07-02 14:28:37 +0530

    chore: add users icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeft, LucideProps, X, User2, HomeIcon, ImageIcon, Link2, MessageSquare, ArrowBigUp, ArrowBigDown, Loader2 } from "lucide-react";
+import { ChevronLeft, LucideProps, X, User2, HomeIcon, ImageIcon, Link2, MessageSquare, ArrowBigUp, ArrowBigDown, Loader2, Users } from "lucide-react";
 
 export const Icons = {
 	logo: (props: LucideProps) => (
@@ -61,5 +61,6 @@ export const Icons = {
 	upvote: ArrowBigUp,
 	downvote: ArrowBigDown,
 	spinner: Loader2,
-	reply: MessageSquare
+	reply: MessageSquare,
+	users: Users
 }
